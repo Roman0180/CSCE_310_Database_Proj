5422a9f648eea67a56707268b9f8d0083fa2f070
Roman <rgasiorowski01@gmail.com>
2022-12-02 17:21:42 -0600

    added backend c# code

---
+++ API/Controllers/ReservationEntityController.cs
@@ -0,0 +1,32 @@
+// using System.Collections;
+// using Microsoft.AspNetCore.Mvc;
+
+// namespace API.Controllers;
+
+// [ApiController]
+// [Route("[controller]")]
+// public class ReservationEntityController : ControllerBase
+// {
+
+//     private readonly ILogger<ReservationEntityController> _logger;
+
+//     // public ReservationController(ILogger<ReservationEntityController> logger)
+//     // {
+//     //     _logger = logger;
+//     // }
+
+//     [HttpGet(Name = "checkReservations")]
+//     public List<Reservation> Get(int restaurantId)
+//     {
+//         ReservationFunctions db = new ReservationFunctions(); 
+//         db.reservationDataFetch(); 
+//         return db.getReservations(restaurantId); 
+//     }
+
+//     // [HttpPut(Name = "registerUser")]
+//     // public void Put(String firstName, String lastName, String email, String password)
+//     // {
+//     //     ReservationFunctions db = new ReservationFunctions(); 
+//     //     db.updateUsers(firstName, lastName, email, password); 
+//     // }
+// }
