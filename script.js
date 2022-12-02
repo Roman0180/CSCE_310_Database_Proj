80ed6b14b2cb7f2774e9dc261f45394041d4a466
joni guerrero <joni@client-10-229-171-107.tamulink.tamu.edu>
2022-12-02 17:22:10 -0600

    changes to home page

---
+++ script.js
@@ -0,0 +1,10 @@
+fetch("http://localhost:8000/")
+
+.then(function(response){
+    return response.json();
+})
+
+.then(function(products){
+    let placeholder = document.querySelector("#");
+    let out = "";
+})
