96a1dbfb351636b55717806fa592f1e32c59cef1
BuildTools <unconfigured@null.spigotmc.org>
2022-12-05 15:27:58 -0600

    Restored branch and added new versions of review for user and employee

---
+++ API/Controllers/ReviewEntityEmployeeController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ReviewEntityEmployeeController : ControllerBase
+{
+
+    private readonly ILogger<ReviewEntityEmployeeController> _logger;
+
+
+    [HttpGet(Name = "checkRestaurantReviews")]
+    public List<Tuple<int, int, string, DateTime,int, string, int>> Get(int restaurantId)
+    {
+        ReviewEmployeeFunctions db = new ReviewEmployeeFunctions(); 
+        db.reviewDataFetch(); 
+        return db.getReviews(restaurantId); 
+    }
+
+    [HttpPut(Name = "updateComment")]
+    public void Put(int comment_id, string comment,int employee_id)
+    {
+        // var cs = "Host=csce-315-db.engr.tamu.edu;Username=csce310_gasiorowski;Password=229001014;Database=csce310_db";
+        // using var conn = new NpgsqlConnection(cs);
+        // conn.Open();
+        // NpgsqlCommand command = new NpgsqlCommand("UPDATE review_entity SET comment = '"+comment+"'WHERE comment_id= "+comment_id+";", conn);
+        // NpgsqlDataReader reader = command.ExecuteReader();
+        // NpgsqlCommand command2 = new NpgsqlCommand("UPDATE review_entity SET employee_id = "+employee_id+"WHERE comment_id= "+comment_id+";", conn);
+        // NpgsqlDataReader reader2 = command2.ExecuteReader();
+        ReviewEmployeeFunctions db = new ReviewEmployeeFunctions(); 
+        db.updateComment(comment_id,comment);
+        db.updateCommentEmployee(comment_id,employee_id);
+       
+    }
+    // [HttpPut(Name = "updateEmployeeId")]
+    // public void Put(int comment_id, int employee_id)
+    // {
+    //     var cs = "Host=csce-315-db.engr.tamu.edu;Username=csce310_gasiorowski;Password=229001014;Database=csce310_db";
+    //     using var conn = new NpgsqlConnection(cs);
+    //     conn.Open();
+    //     NpgsqlCommand command = new NpgsqlCommand("UPDATE review_entity SET employee_id = "+employee_id+"WHERE comment_id= "+comment_id+";", conn);
+    //     NpgsqlDataReader reader = command.ExecuteReader();
+        
+        
+    // }
+}
