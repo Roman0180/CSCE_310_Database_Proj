1d77c4bc81e0796e1a499a43225c7a8e1786f416
joni guerrero <joni@connect-172-31-5-198.vpn.tamu.edu>
2022-12-06 10:29:36 -0600

    log in verifying

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-fetch("http://localhost:8000/")
+firstName = "firstName"
+lastName = "LastName"
 
-.then(function(response){
-    return response.json();
-})
+function userInfo(firstName, lastName){
+    return firstName, lastName
+}
 
-.then(function(products){
-    let placeholder = document.querySelector("#");
-    let out = "";
+function userLogIn() {
+    var nameValue = document.getElementById("floatingUsername").value;
+    nameValue = nameValue.replace("@", "%40")
+    var passwordValue = document.getElementById("floatingPassword").value;
+    url = "https://localhost:7091/UserEntity?userName="+nameValue+"&password="+passwordValue
 
-    for(let product of products){
-        out += `
-            <tr>
-        `
-    }
-})
+    fetch(url).then(response => 
+        response.json().then(data => ({
+            data: data,
+            status: response.status
+        })
+    ).then(res => {
+        // console.log(res.status, res.data.title)
+        console.log(res)
+        if(res.data.item1 == true){
+            firstName = res.data.item3
+            lastName = res.data.item4
+            userInfo(firstName, lastName)
+            window.location.replace("./loggedInIndex.html");
+        }
+        else{
+            window.alert("Wrong username and/or password. Try again.");
+        }
+    }));
+}
