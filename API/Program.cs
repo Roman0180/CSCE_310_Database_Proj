7b8500e8a2b48c9174ca6aa15d67b62cc6856273
BuildTools <unconfigured@null.spigotmc.org>
2022-12-08 18:00:47 -0600

    added review to master

--- API/Program.cs
+++ API/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(policyBuilder =>
     policyBuilder.AddDefaultPolicy(policy =>
-        policy.WithOrigins("http://127.0.0.1:5500", "http://127.0.0.1:5501").AllowAnyHeader().AllowAnyHeader())
+        policy.WithOrigins("http://127.0.0.1:5500", "http://127.0.0.1:5501").AllowAnyHeader().AllowAnyHeader().AllowAnyMethod())
 );
 
 
--- API/ReviewUserFunctions.cs
+++ API/ReviewUserFunctions.cs
@@ -8,30 +8,45 @@ using System.Text.Json;
 
 namespace API;
 
-
+public class ReviewWithCustomerID{
+    public int commentId;
+    public int order_num;
+    public int customerId;
+    public string firstName;
+    public string lastName; 
+    public string firstLast;
+    public int rating;
+    public string text_feedback;
+    public DateTime date_posted; 
+    public int restaurantId; 
+    public string comment;
+    public int employee_id;
+}
 
 public class ReviewUserFunctions
 {
     // restaurant of review -> all reviews at that restaurant
     Dictionary<int, List<Review>> review_table = new Dictionary<int, List<Review>>();
-
+    Dictionary<int, List<ReviewWithCustomerID>> review_table_customer = new Dictionary<int, List<ReviewWithCustomerID>>();
     public void reviewDataFetch()
     {
         var cs = "Host=csce-315-db.engr.tamu.edu;Username=csce310_gasiorowski;Password=229001014;Database=csce310_db";
         using var conn = new NpgsqlConnection(cs);
         conn.Open();
-        NpgsqlCommand command = new NpgsqlCommand("SELECT * from review_entity", conn);
+        NpgsqlCommand command = new NpgsqlCommand("SELECT * from reviewWithNames", conn);
         NpgsqlDataReader reader = command.ExecuteReader();
         while (reader.Read()){
             Review review = new Review(); 
-            review.order_num = reader.GetInt32(1);
-            review.rating = reader.GetInt32(2);
-            review.text_feedback = reader.GetString(3);
-            review.date_posted = reader.GetDateTime(4); 
-            review.restaurantId = reader.GetInt32(5); 
+            review.commentId = reader.GetInt32(0);
+            review.firstName = reader.GetString(1);
+            review.lastName = reader.GetString(2);
+            review.rating = reader.GetInt32(3);
+            review.text_feedback = reader.GetString(4);
+            review.date_posted = reader.GetDateTime(5);
             review.comment = reader.GetString(6);
-            review.employee_id = reader.GetInt32(7);
-            //review.comment_customer_id = reader.GetInt32(8);
+            review.restaurantId = reader.GetInt32(7);
+            review.employee_id = reader.GetInt32(8);
+            review.firstLast = review.firstName + " " + review.lastName;
             if(review_table.ContainsKey(review.restaurantId)){
                 review_table[review.restaurantId].Add(review);
             } 
@@ -42,12 +57,62 @@ public class ReviewUserFunctions
             }
         }
     }
-    public List<Tuple<int, int, string, DateTime, int, string, int>> getReviews(int restaurantId){
-        List<Tuple<int, int, string, DateTime, int, string, int>> reviewData= new List<Tuple<int, int, string, DateTime, int, string, int>>(); 
+
+
+    public void reviewDataFetchVariant()
+        {
+            var cs = "Host=csce-315-db.engr.tamu.edu;Username=csce310_gasiorowski;Password=229001014;Database=csce310_db";
+            using var conn = new NpgsqlConnection(cs);
+            conn.Open();
+            NpgsqlCommand command = new NpgsqlCommand("SELECT * from reviewWithNamesAndCustomerId", conn);
+            NpgsqlDataReader reader = command.ExecuteReader();
+            while (reader.Read()){
+                ReviewWithCustomerID review = new ReviewWithCustomerID(); 
+                review.commentId = reader.GetInt32(0);
+                review.customerId = reader.GetInt32(1);
+                review.order_num = reader.GetInt32(2);
+                review.firstName = reader.GetString(3);
+                review.lastName = reader.GetString(4);
+                review.rating = reader.GetInt32(5);
+                review.text_feedback = reader.GetString(6);
+                review.date_posted = reader.GetDateTime(7);
+                review.comment = reader.GetString(8);
+                review.restaurantId = reader.GetInt32(9);
+                review.employee_id = reader.GetInt32(10);
+                review.firstLast = review.firstName + " " + review.lastName;
+                if(review_table_customer.ContainsKey(review.customerId)){
+                    review_table_customer[review.customerId].Add(review);
+                } 
+                else{
+                    List<ReviewWithCustomerID> newList = new List<ReviewWithCustomerID>(); 
+                    newList.Add(review); 
+                    review_table_customer.Add(review.customerId, newList);
+                }
+            }
+        }
+
+
+    public List<Tuple<int,string,int,string,DateTime,string,int>> getReviews(int restaurantId){
+        List<Tuple<int,string,int,string,DateTime,string,int>> reviewData= new List<Tuple<int,string,int,string,DateTime,string,int>>(); 
 
         if (review_table.ContainsKey(restaurantId)){
             foreach(var review in review_table[restaurantId]){
-                Tuple<int, int, string, DateTime, int, string, int> r = new Tuple<int, int, string, DateTime, int, string, int>(review.order_num,review.rating, review.text_feedback, review.date_posted, review.restaurantId, review.comment, review.employee_id); 
+                Tuple<int,string,int,string,DateTime,string,int> r = new Tuple<int,string,int,string,DateTime,string,int>(review.commentId,review.firstLast,review.rating,review.text_feedback,review.date_posted,review.comment,review.employee_id); 
+                reviewData.Add(r); 
+            }
+            return reviewData; 
+        }
+        else{
+            return null; 
+        }
+    }
+
+    public List<Tuple<int,int,int,string,DateTime,string,int>> getReviewsByCustomer(int customerId){
+        List<Tuple<int,int,int,string,DateTime,string,int>> reviewData= new List<Tuple<int,int,int,string,DateTime,string,int>>(); 
+
+        if (review_table_customer.ContainsKey(customerId)){
+            foreach(var review in review_table_customer[customerId]){
+                Tuple<int,int,int,string,DateTime,string,int> r = new Tuple<int,int,int,string,DateTime,string,int>(review.commentId,review.order_num,review.rating,review.text_feedback,review.date_posted,review.comment,review.customerId); 
                 reviewData.Add(r); 
             }
             return reviewData; 
@@ -84,6 +149,42 @@ public class ReviewUserFunctions
         else{
             return null;
         }
+    }
+
+    public void updateRating(int commentId, int rating){
+        var cs = "Host=csce-315-db.engr.tamu.edu;Username=csce310_gasiorowski;Password=229001014;Database=csce310_db";
+        using var conn = new NpgsqlConnection(cs);
+        conn.Open();
+        NpgsqlCommand command = new NpgsqlCommand("UPDATE review_entity SET rating = "+rating+"WHERE comment_id= "+commentId+";", conn);
+        NpgsqlDataReader reader = command.ExecuteReader();
+    }
+
+    public void updateFeedback(int commentId, string feedback){
+        var cs = "Host=csce-315-db.engr.tamu.edu;Username=csce310_gasiorowski;Password=229001014;Database=csce310_db";
+        using var conn = new NpgsqlConnection(cs);
+        conn.Open();
+        NpgsqlCommand command = new NpgsqlCommand("UPDATE review_entity SET text_feedback = '"+feedback+"' WHERE comment_id= "+commentId+";", conn);
+        NpgsqlDataReader reader = command.ExecuteReader();
+    }
+
+    public void updateTime(int commentId){
+        var cs = "Host=csce-315-db.engr.tamu.edu;Username=csce310_gasiorowski;Password=229001014;Database=csce310_db";
+        using var conn = new NpgsqlConnection(cs);
+        conn.Open();
+        //UPDATE placed_order_entity SET order_date = (SELECT NOW()::TIMESTAMP) WHERE order_num =1;
+
+        NpgsqlCommand command = new NpgsqlCommand("UPDATE review_entity SET date_posted = (SELECT NOW()::TIMESTAMP) WHERE comment_id = "+commentId+";", conn);
+        NpgsqlDataReader reader = command.ExecuteReader();
+    }
+
+    public void deleteComment(int commentId){
+        var cs = "Host=csce-315-db.engr.tamu.edu;Username=csce310_gasiorowski;Password=229001014;Database=csce310_db";
+        using var conn = new NpgsqlConnection(cs);
+        conn.Open();
+        //UPDATE placed_order_entity SET order_date = (SELECT NOW()::TIMESTAMP) WHERE order_num =1;
+        //DELETE FROM review_entity WHERE comment_id = "+commentId+";"
+        NpgsqlCommand command = new NpgsqlCommand("DELETE FROM review_entity WHERE comment_id = "+commentId+";", conn);
+        NpgsqlDataReader reader = command.ExecuteReader();
     }
 
 
